029fcd511e44c6e3fd692939485e4d53e4af53c2
ChumaSuey <hyperghostblade@gmail.com>
2024-05-31 16:20:03 -0400

    - Implemented trigger_textstory. - Save button is missing. - Preview text is missing.

--- GUI.py
+++ GUI.py
@@ -49,7 +49,7 @@ def copy():
     newline_label.config(text="")
 
 def copy_trenchbroom():
-    """Copies the translated text to the clipboard in the format you specified."""
+    """Copies the translated text to the clipboard in the format of a trigger_relay (point entity)."""
     translated_text = output_field.get("1.0", tk.END)
     if translated_text:
         translated_text = translated_text.rstrip('\n')
@@ -65,6 +65,30 @@ def copy_trenchbroom():
         print("No translated text available to copy.")
     newline_label.config(text="")
 
+def copy_trenchbroom2():
+    """Copies the translated text to the clipboard in the format of a trigger_textstory (brush entity)."""
+    translated_text = output_field.get("1.0", tk.END)
+    if translated_text:
+        translated_text = translated_text.rstrip('\n')
+        translated_text2 = '// entity 0\n{\n'
+        translated_text2 += '  "classname" "trigger_textstory"\n'
+        translated_text2 += '  "message" "' + translated_text + '"\n'
+        translated_text2 += '// brush 0\n{\n'
+        translated_text2 += '( -96 -144 64 ) ( -96 -143 64 ) ( -96 -144 65 ) trigger [ 0 -1 0 0 ] [ 0 0 -1 0 ] 0 1 1\n'
+        translated_text2 += '( -112 -112 64 ) ( -112 -112 65 ) ( -111 -112 64 ) trigger [ 1 0 0 0 ] [ 0 0 -1 0 ] 0 1 1\n'
+        translated_text2 += '( -112 -144 32 ) ( -111 -144 32 ) ( -112 -143 32 ) trigger [ -1 0 0 0 ] [ 0 -1 0 0 ] 0 1 1\n'
+        translated_text2 += '( -48 -64 80 ) ( -48 -63 80 ) ( -47 -64 80 ) trigger [ 1 0 0 0 ] [ 0 -1 0 0 ] 0 1 1\n'
+        translated_text2 += '( -48 -64 80 ) ( -47 -64 80 ) ( -48 -64 81 ) trigger [ -1 0 0 0 ] [ 0 0 -1 0 ] 0 1 1\n'
+        translated_text2 += '( -48 -64 80 ) ( -48 -64 81 ) ( -48 -63 80 ) trigger [ 0 1 0 0 ] [ 0 0 -1 0 ] 0 1 1\n'
+        translated_text2 += '}\n'  # Add the closing curly brace here
+        absoluteshambler = translated_text2
+        pyperclip.copy(absoluteshambler)
+        print('The text to be copied to the clipboard')
+    else:
+        print("No translated text available to copy.")
+    newline_label.config(text="")
+
+
 root = tk.Tk()
 root.title("Quake Trigger Text Converter")
 
@@ -76,6 +100,10 @@ preview_label.grid(row=0, column=2, padx=10, pady=10, sticky="nsew")
 
 text_field = tk.Text(root, height=10, width=50, font=("Arial", 14))
 text_field.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
+
+copy_save_row = tk.Frame(root)
+copy_save_row.grid(row=3, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
+
 
 scrollbar = tk.Scrollbar(root)
 scrollbar.grid(row=0, column=1, sticky="nsew")
@@ -92,10 +120,13 @@ save_button = ttk.Button(root, text="Save", command=save_file)
 save_button.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
 
 copy_button = ttk.Button(root, text="Copy", command=copy)
-copy_button.grid(row=2, column=1, padx=10, pady=10, sticky="nsew")
+copy_button.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
 
 copy_trenchbroom_button = ttk.Button(root, text="Copy into trigger_relay", command=copy_trenchbroom)
-copy_trenchbroom_button.grid(row=2, column=0, padx=10, pady=10, sticky="nsew")
+copy_trenchbroom_button.grid(row=2, column=1, padx=10, pady=10, sticky="nsew")
+
+copy_trenchbroom_button2 = tk.Button(root, text="Copy into trigger_textstory", command=copy_trenchbroom2)
+copy_trenchbroom_button2.grid(row=2, column=2, padx=10, pady=10, sticky="nsew")
 
 preview_label.grid(row=1, column=0, columnspan=2, padx=10, pady=10, sticky="nsew")
 
